6a5d8e7cc58a850d2aa0d879797cf80958372022
suweih <475464842@qq.com>
2021-05-30 11:57:03 +0800

    deploy

---
+++ precache-manifest.5982b87414edaf9f38ecc24189d1bed7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39f437cea8e4c1149146",
+    "url": "/morney-website/css/app.c2ddf94d.css"
+  },
+  {
+    "revision": "f2a3deee382d86b27072551de5afee16",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "39f437cea8e4c1149146",
+    "url": "/morney-website/js/app.c0e7b8a2.js"
+  },
+  {
+    "revision": "5dcae19c1258cf132c32",
+    "url": "/morney-website/js/chunk-vendors.c05d5144.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
